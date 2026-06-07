62fd0fc4a91f3a958964a08bcc3a78d1c4578fcd
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-06-07 22:49:14 +0200

    feat: first commit with classes of world, movableObj, chicken, character and placed figures into canavas

---
+++ js/models/movable-object.class.js
@@ -0,0 +1,24 @@
+class MovableObject {
+    x = 120;
+    y = 250;
+    img;
+    height = 150;
+    width = 100;
+
+    loadImage(path) {
+        this.img = new Image() // this.img = document.getElementById('image') <img id="image" src="">
+        this.img.src = path;
+
+    }
+
+    moveRight() {
+        console.log("Moving-right");
+    }
+
+
+    moveLeft() {
+
+    }
+
+
+}
