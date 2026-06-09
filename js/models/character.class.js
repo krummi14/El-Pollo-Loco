9087a72d2187c186a444705635a4330c73e5abc9
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-06-09 10:21:32 +0200

    feat: add animation of character, cloud and chichens

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -2,12 +2,32 @@ class Character extends MovableObject {
 
     height = 300;
     y = 140;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+    
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.animateCharacter();
     }
-     jump() {
+
+    animateCharacter() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+    }
 
 
+    jump() {
 
     }
 }
--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -4,21 +4,37 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.25;
 
     loadImage(path) {
         this.img = new Image() // this.img = document.getElementById('image') <img id="image" src="">
         this.img.src = path;
+    }
 
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png, ...] 
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
         console.log("Moving-right");
     }
 
-
     moveLeft() {
-
+        setInterval(() => {
+            this.x -= this.speed;
+            if (this.x + this.width < 0) {
+                this.x = this.width;
+            }
+        }, 1000 / 60);
     }
-
-
 }
